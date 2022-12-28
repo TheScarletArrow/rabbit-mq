f8d43c0b5c3030e3edffd5ecec60ce1369ba70e0
Anton Yurkov <anton20001701@yandex.ru>
2022-12-28 17:22:51 +0300

    Initial commit

---
+++ src/main/java/ru/scarlet/rabbit/KotlinRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package ru.scarlet.rabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotlinRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KotlinRabbitmqApplication.class, args);
+	}
+
+}
